b726ac43a1937916e9b53f8ce0d6b70516a4b1ad
aveinber <aveinber@users.noreply.github.com>
2016-05-05 23:32:29 +0300

    YL13

--- funk.php
+++ funk.php
@@ -10,45 +10,92 @@ function connect_db(){
 	$connection = mysqli_connect($host, $user, $pass, $db) or die("ei saa ühendust mootoriga- ".mysqli_error());
 	mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
 }
-function logi(){
-	// siia on vaja funktsionaalsust (13. nädalal)
-
-	include_once('views/login.html');
-	
-}
-function logout(){
-	// siia on vaja funktsionaalsust (13. nädalal)
-	
-	include_once('views/v2rav.html');
-	
-}
 
 
 function kuva_puurid(){
-	//  db ühendus
+	// siia on vaja funktsionaalsust
+	if(!empty($_SESSION['user'])){
+	
 	global $connection;
-	// loome tühja massiivi
+	$p= mysqli_query($connection, "select distinct(puur) as puur from aveinber_loomaaed order by puur asc");
 	$puurid=array();
-	//käivitame ainulaadsete puuride päringu tabelist aveinber_loomaaed
-	$sql= "select distinct(puur) from aveinber_loomaaed";
-	$query= mysqli_query($connection,$sql);
-	//ainulaadsetesse puuridesse vastavad loomad
-	while ($puuri_nr=mysqli_fetch_assoc($query)){
-		$query2=mysqli_query($connection, "select * from aveinber_loomaaed where  puur=".mysqli_real_escape_string($connection, $puuri_nr['puur']));
-		while ($loomarida=mysqli_fetch_assoc($query2)) {
-			$puurid[$puuri_nr['puur']][]=$loomarida;
+	while ($r=mysqli_fetch_assoc($p)){
+		$l=mysqli_query($connection, "SELECT * FROM aveinber_loomaaed WHERE  puur=".mysqli_real_escape_string($connection, $r['puur']));
+		while ($row=mysqli_fetch_assoc($l)) {
+			$puurid[$r['puur']][]=$row;
 		}
 	}
-	
 	include_once('views/puurid.html');
+	}
+	if(empty($_SESSION['user']))
+	{
+		include_once('views/login.html');
+	}
+}	
 	
-}
+	
+function logi(){
+	// siia on vaja funktsionaalsust (13. nädalal)
+    $errors=array();
+	
+       if($_SERVER['REQUEST_METHOD'] == "POST"){
+			if(in_array("", $_POST)){
+				$errors[] = "Kasutajanimi ja/või parool täita!";
+				include_once('views/login.html');
+			}else{
+			     $errors[] = "Kasutajanimi ja/või parool on vale!";
+				global $connection;
+	           $username=mysqli_real_escape_string($connection, $_POST['username']);
+	           $password=mysqli_real_escape_string($connection, $_POST['password']);
+			   $query ="SELECT id, username FROM aveinber_kylastajad WHERE username='$username' and passw=sha1('$password')";
+			   $result = mysqli_query($connection, $query) or die("Ei saanud päringut teha ".mysqli_error($connection));
+			   $kasutajarida=mysqli_fetch_assoc($result);
+			   if($kasutajarida['username']== $_POST['username']){
+				  $_SESSION['user']=$username;
+				  header('Location:loomaaed.php?page=loomad');   
+			   }
+			include_once('views/login.html');
+		}
+	}else{
+		include_once('views/login.html');
+		}
+	}
 
+
+
+function logout(){
+	$_SESSION=array();
+	session_destroy();
+	header("Location: ?");
+}
 
 function lisa(){
 	// siia on vaja funktsionaalsust (13. nädalal)
-	
-	include_once('views/loomavorm.html');
+	$errors=array();
+	if(!empty($_SESSION['username'])){
+		include_once('views/login.html');
+	} else
+	{  
+       if($_SERVER['REQUEST_METHOD'] == "POST"){
+			if(in_array("", $_POST)){
+				$errors[] = "Väljad täita!";
+				include_once('views/loomavorm.html');
+			}else{
+			    global $connection;
+				upload('liik');
+	            $nimi=mysqli_real_escape_string($connection, $_POST['nimi']);
+	            $puur=mysqli_real_escape_string($connection, $_POST['puur']);
+			    $liik=mysqli_real_escape_string($connection, "pildid/".$_FILES["liik"]["name"]);
+			    $query ="INSERT INTO aveinber_loomaaed (nimi, puur, liik ) values ('$nimi','$puur','$liik')";
+			    $result = mysqli_query($connection, $query) or die("Ei saanud päringut teha ".mysqli_error($connection));
+			   
+				header('Location:loomaaed.php?page=loomad');   
+			  
+		}
+	}else{
+		include_once('views/loomavorm.html');
+		}
+	}
 	
 }
 
